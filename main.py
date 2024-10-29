fd2c28c357eb8f2f8797de8c5d71880f32251b09
Quateem Gaffney-Smith <90237467+qgaffney@users.noreply.github.com>
2024-10-29 13:42:04 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#Create class Student
+class Student:
+  #Constructor
+  def __intit__ (self, studentName, studentID, studentScore):
+    self.studentName = name
+    self.studentID = studentID
+    self.studentScore = studentScore
+  #Function for new students
+  def add(self, studentName, studentID, studentScore):
+
+    ob = Student(name, studentID, studentScore)
+    ls.append(ob)
+
